b8b13ff86663320221f9fdf54bddce69e9e52388
4lkaid <4lkaid@gmail.com>
2024-12-13 15:26:29 +0800

    refactor: simplify middleware

--- src/route/api.rs
+++ src/route/api.rs
@@ -1,6 +1,5 @@
 use crate::handler;
 use axum::{
-    middleware,
     routing::{get, post},
     Router,
 };
@@ -27,8 +26,6 @@ pub fn init() -> Router {
                 .layer(request_id::propagate_request_id())
                 .layer(trace::trace())
                 .layer(cors::cors())
-                .layer(middleware::from_fn(
-                    request_response_logger::print_request_response,
-                )),
+                .layer(request_response_logger::print()),
         )
 }
